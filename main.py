2e128a1a577d2f54fb6be3e31a5405e8b8b8e868
Eric <schaikejgv@gmail.com>
2018-09-06 18:46:08 +0200

    PyQt5 works finally!!

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import sys
 from PyQt5 import QtWidgets
 
+
 def window():
     app = QtWidgets.QApplication(sys.argv)
-
-    window = QtWidgets.QWidget()
-
-    window.setWindowTitle("Hello World")
-
-    window.setGeometry(50, 50, 500, 500)
-
-    window.show()
-
+    w = QtWidgets.QWidget()
+    b = QtWidgets.QLabel(w)
+    b.setText("Hello World!")
+    w.setGeometry(100, 100, 200, 50)
+    w.show()
     sys.exc_info(app.exec_())
 
-window()
+
+if __name__ == '__main__':
+    window()
