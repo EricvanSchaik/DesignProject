c16e4558a67a9c7d5f477b6188cdc8efa4dc1d33
Eric <schaikejgv@gmail.com>
2018-09-08 09:48:20 +0200

    PyQt5 works finally!!

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QLineEdit, QTextEdit
 
 
-def window():
-    app = QtWidgets.QApplication(sys.argv)
-    w = QtWidgets.QWidget()
-    b = QtWidgets.QLabel(w)
-    b.setText("Hello World!")
-    w.setGeometry(100, 100, 200, 50)
-    w.show()
-    sys.exc_info(app.exec_())
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+        grid = QGridLayout()
+        grid.setSpacing(10)
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')
+        self.show()
 
 
 if __name__ == '__main__':
-    window()
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
