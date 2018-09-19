2dd922e7e77900f92be4ef62fd508f1351aeb12d
Dennis Cai <d.cai@student.utwente.nl>
2018-09-19 14:51:23 +0200

    Initial machine learning test code.

---
+++ machine_learning/test.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
+
+# Importing dataset
+data = pd.read_csv("train.csv")
+
+# Convert categorical variable to numeric
+data["Sex_cleaned"] = np.where(data["Sex"] == "male", 0, 1)
+data["Embarked_cleaned"] = np.where(data["Embarked"] == "S", 0,
+                                    np.where(data["Embarked"] == "C", 1,
+                                             np.where(data["Embarked"] == "Q", 2, 3)
+                                             )
+                                    )
+
+# Cleaning dataset of NaN
+data = data[[
+    "Survived",
+    "Pclass",
+    "Sex_cleaned",
+    "Age",
+    "SibSp",
+    "Parch",
+    "Fare",
+    "Embarked_cleaned"
+]].dropna(axis=0, how='any')
+
+# Split dataset in training and test datasets
+X_train, X_test = train_test_split(data, test_size=0.5, random_state=int(time.time()))
+
+gnb = GaussianNB()
+used_features = [
+    "Pclass",
+    "Sex_cleaned",
+    "Age",
+    "SibSp",
+    "Parch",
+    "Fare",
+    "Embarked_cleaned"
+]
+
+# Train classifier
+gnb.fit(
+    X_train[used_features],
+    X_train["Survived"]
+)
+
+y_pred = gnb.predict(X_test[used_features])
+
+# Print results
+print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%".format(
+    X_test.shape[0],
+    (X_test["Survived"] != y_pred).sum(),
+    100 * (1 - (X_test["Survived"] != y_pred).sum() / X_test.shape[0])
+))
+
+# mean_survival = np.mean(X_train["Survived"])
+# mean_not_survival = 1 - mean_survival
+#
+# print("Survival prob = {:03.2f}%, Not survival prob = {:03.2f}%".format(
+#     100 * mean_survival,
+#     100 * mean_not_survival
+# ))
+#
+# mean_fare_survived = np.mean(X_train[X_train["Survived"] == 1]["Fare"])
+# std_fare_survived = np.std(X_train[X_train["Survived"] == 1]["Fare"])
+# mean_fare_not_survived = np.mean(X_train[X_train["Survived"] == 0]["Fare"])
+# std_fare_not_survived = np.std(X_train[X_train["Survived"] == 0]["Fare"])
+#
+# print("mean_fare_survived = {:03.2f}".format(mean_fare_survived))
+# print("std_fare_survived = {:03.2f}".format(std_fare_survived))
+# print("mean_fare_not_survived = {:03.2f}".format(mean_fare_not_survived))
+# print("std_fare_not_survived = {:03.2f}".format(std_fare_not_survived))
+
+
+# # Gaussian Naive Bayes
+# from sklearn import datasets
+# from sklearn import metrics
+# from sklearn.naive_bayes import GaussianNB
+#
+# # load the iris datasets
+# dataset = datasets.load_iris()
+#
+# # fit a Naive Bayes model to the data
+# model = GaussianNB()
+#
+# model.fit(dataset.data, dataset.target)
+#
+# print(model)
+#
+# # make predictions
+# expected = dataset.target
+# predicted = model.predict(dataset.data)
+#
+# # summarize the fit of the model
+# print(metrics.classification_report(expected, predicted))
+# print(metrics.confusion_matrix(expected, predicted))
