b54ddc860f22048057173d61ce6a68f2b0e0460a
Martijn <martijnrenssen@live.nl>
2018-09-21 14:44:41 +0200

    Documentation and loading previous data on class initialization

--- datastorage/Preferences.py
+++ datastorage/Preferences.py
@@ -3,11 +3,16 @@ import pickle
 
 class Preferences:
 
+    # TODO: add project folder to path
     file_path = "preferences.pkl"
-    video_path = ""
-    data_path = ""
+    video_path = None
+    data_path = None
+
+    def __init__(self):
+        self.load()
 
     def load(self):
+        """Loads the saved values back into this class from a file"""
         f = open(self.file_path, 'rb')
         tmp_dict = pickle.load(f)
         f.close()
@@ -15,6 +20,7 @@ class Preferences:
         self.__dict__.update(tmp_dict)
 
     def save(self):
+        """Saves the values inside this class to a file"""
         f = open(self.file_path, 'wb')
         pickle.dump(self.__dict__, f)
         f.close()
@@ -22,7 +28,6 @@ class Preferences:
 
 if __name__ == '__main__':
     p = Preferences()
-    # p.video_path = "data/videos"
+    # p.video_path = "test/videos"
     # p.save()
-    p.load()
     print(p.video_path)
