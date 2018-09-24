02172d8dd3c7c7b1361727661a4d8761f6a8c5c3
Semakon <Semakon@gmail.com>
2018-09-24 15:42:27 +0200

    Generalization of importing data from a CSV

--- data_import/sensor_data.py
+++ data_import/sensor_data.py
@@ -5,80 +5,107 @@ from data_import import sensor as sens
 from data_import import column_metadata as cm
 
 
+def parse_header_option(file, row_nr, col_nr):
+    """
+    Parses a specific part of the header with a line number and a column number
+    :param file: file to be parsed
+    :param row_nr: row number of header
+    :param col_nr: column number of header data
+    :return: data on row and column number
+    """
+    i = 1  # row numbers start at 1
+    for line in file:
+        if i == row_nr:
+            return re.split(', *', line)[col_nr + 1]  # column numbers start at 1
+        else:
+            i += 1
+    # error
+    return -1
+
+
+def parse_names(file, row_nr):
+    """
+    Parses the names of the data columns using a row number
+    :param file: file to be parsed
+    :param row_nr: row number of names
+    :return: list of column names
+    """
+    i = 1
+    for line in file:
+        if i == row_nr:
+            return re.split(', *', line[1:-1])
+        else:
+            i += 1
+    # error
+    return -1
+
+
 class SensorData:
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, settings):
+        # Initiate primitives
         self.file_path = file_path
-        self.headers = self.parse_headers()
-        self.data = self.parse_data()
-        self.metadata = []
+        self.metadata = dict()
+        self.names = []
+        self.col_metadata = []
 
-    def parse_headers(self):
-        """
-        Parses a csv file to get the headers. Ignores the data.
-        :return: a 2D list with information on the sensor and its data.
-        """
+        # Parse metadata and data
+        self.data = self.parse(settings)
 
-        # create list of headers
-        headers = []
-
-        # Add headers to header list
+    def parse(self, settings):
+        # Parse metadata from headers
         with open(self.file_path) as file:
-            for line in file:
-                if line.startswith(";"):
-                    # semicolon at start of line indicates a comment, ergo a header
-                    # remove it and convert line from string to list of arguments
-                    headers.append(re.split(', *', line[1:-1]))
-                else:
-                    # end of header section
-                    break
-        return headers
+            self.metadata['time'] = parse_header_option(file, settings['time_row'], settings['time_col'])
+            self.metadata['date'] = parse_header_option(file, settings['date_row'], settings['date_col'])
+            self.metadata['sr'] = parse_header_option(file, settings['sr_row'], settings['sr_col'])
+            self.metadata['sn'] = parse_header_option(file, settings['sn_row'], settings['sn_col'])
+            self.names = parse_names(file, settings['names_row'])
 
-    def parse_data(self):
-        """
-        Parses a csv file to get its data. Ignores the headers
-        :return: a DataFrame with this sensor's data
-        """
-        # Get names of columns
-        names = self.get_names()
-
-        # Parse data
-        data = pd.read_csv(self.file_path, header=None, names=names, comment=';')
+        # Parse data from file
+        data = pd.read_csv(self.file_path, header=None, names=self.names, comment=settings.comment)
 
         # TODO: generalise using column metadata:
         # Convert sensor data to correct unit
         # Accelerometer to m/s
-        f.column_operation(data, names[1], f.mul, 9.807 / 4096)
-        f.column_operation(data, names[2], f.mul, 9.807 / 4096)
-        f.column_operation(data, names[3], f.mul, 9.807 / 4096)
+        f.column_operation(data, self.names[1], f.mul, 9.807 / 4096)
+        f.column_operation(data, self.names[2], f.mul, 9.807 / 4096)
+        f.column_operation(data, self.names[3], f.mul, 9.807 / 4096)
 
         # Gyroscope data to ?/s
-        f.column_operation(data, names[4], f.div, 16.384)
-        f.column_operation(data, names[5], f.div, 16.384)
-        f.column_operation(data, names[6], f.div, 16.384)
+        f.column_operation(data, self.names[4], f.div, 16.384)
+        f.column_operation(data, self.names[5], f.div, 16.384)
+        f.column_operation(data, self.names[6], f.div, 16.384)
 
         # Magnetometer to ?T (micro Tesla)
-        f.column_operation(data, names[7], f.div, 3.413)
-        f.column_operation(data, names[8], f.div, 3.413)
-        f.column_operation(data, names[9], f.div, 3.413)
+        f.column_operation(data, self.names[7], f.div, 3.413)
+        f.column_operation(data, self.names[8], f.div, 3.413)
+        f.column_operation(data, self.names[9], f.div, 3.413)
 
         # Temperature to C
-        f.column_operation(data, names[10], f.div, 1000)
+        f.column_operation(data, self.names[10], f.div, 1000)
 
         # Return data
         return data
 
-    def set_metadata(self):
+    def set_column_metadata(self, settings):
         """
         Sets the metadata for every column
         """
-        for name in self.get_names():
-            # TODO: parse data_type, sensor, and conversion from headers automatically
+        for name in self.names:
             # parse data_type
-            data_type = None
+            data_type = settings[name + "_data_type"]
 
-            # parse sensor
-            # parse conversion rate (part of sensor)
+            # parse sensor:
+            # sensor name
+            sensor_name = settings[name + "_sensor_name"]
+
+            # sampling rate
+            sr = settings[name + "_sampling_rate"]
+
+            # unit of measurement
+            unit = settings[name + "_unit"]
+
+            # TODO: parse conversion rate automatically with a given function
             conversion = None
             if name.startswith("A"):  # Accelerometer
                 conversion = 9.807 / 4096
@@ -89,41 +116,8 @@ class SensorData:
             elif name == "T":  # Temperature
                 conversion = 1000
 
-            sensor = sens.Sensor(name, None, None, conversion)
+            # construct sensor
+            sensor = sens.Sensor(sensor_name, sr, unit, conversion)
 
             # create new column metadata and add it to list with metadata
-            self.metadata.append(cm.ColumnMetadata(name, data_type, sensor))
-
-    def get_names(self):
-        """
-        Retrieves the names of the columns
-        :return: names of the columns
-        """
-        if len(self.headers) < 1:
-            return -1
-        return self.headers[-1]
-
-    def get_header(self, prefix):
-        """
-        Finds the header with the given prefix.
-        :param prefix: prefix of the header
-        :return: the header corresponding to the prefix
-        """
-        for h in self.headers:
-            if h[0].lower() == prefix.lower():
-                return h[1:]
-        return -1
-
-    def get_time(self):
-        """
-        Retrieves the date and start time of the sensor.
-        :return: List containing the date and start time of the sensor
-        """
-        return self.get_header('Start_time')
-
-    def get_serial_number(self):
-        """
-        Retrieves the serial number of the sensor.
-        :return: Serial Number of used sensor
-        """
-        return self.get_header('Version')[3]
+            self.col_metadata.append(cm.ColumnMetadata(name, data_type, sensor))
