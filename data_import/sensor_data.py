671d72e06d6b1130eb20979735b0fb9ebc217e1f
Semakon <Semakon@gmail.com>
2018-09-25 13:56:06 +0200

    fixed some bugs

--- data_import/sensor_data.py
+++ data_import/sensor_data.py
@@ -1,8 +1,6 @@
 import re
 import pandas as pd
-from data_import import function as f
-from data_import import sensor as sens
-from data_import import column_metadata as cm
+from data_import import sensor as sens, column_metadata as cm
 
 
 def parse_header_option(file, row_nr, col_nr):
@@ -13,10 +11,13 @@ def parse_header_option(file, row_nr, col_nr):
     :param col_nr: column number of header data
     :return: data on row and column number
     """
+    # return to start of file
+    file.seek(0)
+
     i = 1  # row numbers start at 1
     for line in file:
         if i == row_nr:
-            return re.split(', *', line)[col_nr + 1]  # column numbers start at 1
+            return re.split(', *', line)[col_nr - 1]  # column numbers start at 1
         else:
             i += 1
     # error
@@ -30,6 +31,9 @@ def parse_names(file, row_nr):
     :param row_nr: row number of names
     :return: list of column names
     """
+    # return to start of file
+    file.seek(0)
+
     i = 1
     for line in file:
         if i == row_nr:
@@ -56,6 +60,8 @@ class SensorData:
             - sn_row, sn_col       (row and column where 'serial number' attribute is located)
             - names_row            (row where the names of the columns are located)
 
+            - comment              (symbol used to indicate a comment !this is a value, not a location!)
+
         The next keys are variable depending on the name of the column (these are not locations, but values!):
 
             - <name>_data_type     (data type of the column)
@@ -67,7 +73,7 @@ class SensorData:
         self.file_path = file_path
         self.metadata = dict()
         self.names = []
-        self.col_metadata = []
+        self.col_metadata = dict()
 
         # Parse metadata and data
         self.data = self.parse(settings)
@@ -90,29 +96,19 @@ class SensorData:
         self.set_column_metadata(settings)
 
         # Parse data from file
-        data = pd.read_csv(self.file_path, header=None, names=self.names, comment=settings.comment)
+        data = pd.read_csv(self.file_path, header=None, names=self.names, comment=settings['comment'])
 
-        # TODO: generalise using column metadata:
         # Convert sensor data to correct unit
-        # Accelerometer to m/s
-        f.column_operation(data, self.names[1], f.mul, 9.807 / 4096)
-        f.column_operation(data, self.names[2], f.mul, 9.807 / 4096)
-        f.column_operation(data, self.names[3], f.mul, 9.807 / 4096)
+        for name in self.names:
+            conversion = self.col_metadata[name].sensor.conversion
 
-        # Gyroscope data to ?/s
-        f.column_operation(data, self.names[4], f.div, 16.384)
-        f.column_operation(data, self.names[5], f.div, 16.384)
-        f.column_operation(data, self.names[6], f.div, 16.384)
+            # If column doesn't have a conversion, continue to next column
+            if conversion is None:
+                continue
 
-        # Magnetometer to ?T (micro Tesla)
-        f.column_operation(data, self.names[7], f.div, 3.413)
-        f.column_operation(data, self.names[8], f.div, 3.413)
-        f.column_operation(data, self.names[9], f.div, 3.413)
+            # Apply conversion to the data
+            data[name] = data[name].apply(lambda x: x * conversion)
 
-        # Temperature to C
-        f.column_operation(data, self.names[10], f.div, 1000)
-
-        # Return data
         return data
 
     def set_column_metadata(self, settings):
@@ -138,14 +134,14 @@ class SensorData:
             if name.startswith("A"):  # Accelerometer
                 conversion = 9.807 / 4096
             elif name.startswith("G"):  # Gyroscope
-                conversion = 16.384
+                conversion = 1 / 16.384
             elif name.startswith("M"):  # Magnetometer
-                conversion = 3.413
+                conversion = 1 / 3.413
             elif name == "T":  # Temperature
-                conversion = 1000
+                conversion = 1 / 1000
 
             # construct sensor
             sensor = sens.Sensor(sensor_name, sr, unit, conversion)
 
             # create new column metadata and add it to list with metadata
-            self.col_metadata.append(cm.ColumnMetadata(name, data_type, sensor))
+            self.col_metadata[name] = cm.ColumnMetadata(name, data_type, sensor)
