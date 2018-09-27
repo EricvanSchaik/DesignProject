3f43870692b0c601ad43f64b352d5e80598491d2
Martijn <martijnrenssen@live.nl>
2018-09-27 15:32:50 +0200

    Store end time of labels

--- datastorage/LabelStorage.py
+++ datastorage/LabelStorage.py
@@ -5,13 +5,13 @@ sql_new_label = "INSERT INTO labelType(Name, Color, Description) VALUES (?,?,?)"
 sql_del_label_type = "DELETE FROM labelType WHERE Name = ?"
 sql_del_label_data_all = "DELETE FROM labelData WHERE Label_name = ?"
 sql_del_label_data = "DELETE FROM labelData WHERE Start_time = ? AND Sensor_id = ?"
-sql_add_label = "INSERT INTO labelData(Start_time, Label_name, Sensor_id) VALUES (?,?,?)"
+sql_add_label = "INSERT INTO labelData(Start_time, End_time, Label_name, Sensor_id) VALUES (?,?,?,?)"
 sql_upd_name_type = "UPDATE labelType SET Name = ? WHERE Name = ?"
 sql_upd_name_data = "UPDATE labelData SET Label_name = ? WHERE Label_name = ?"
 sql_upd_color = "UPDATE labelType SET Color = ? WHERE Name = ?"
 sql_upd_desc = "UPDATE labelType SET Description = ? WHERE Name = ?"
 sql_change_label = "UPDATE labelData SET Label_name = ? WHERE Start_time = ? AND Sensor_id = ?"
-sql_get_labels = "SELECT Start_time, Label_name FROM labelData WHERE Sensor_id = ?"
+sql_get_labels = "SELECT Start_time, End_time, Label_name FROM labelData WHERE Sensor_id = ?"
 
 
 class LabelManager:
@@ -27,7 +27,7 @@ class LabelManager:
         """Method for creating the necessary label tables in the database."""
         c = self._conn.cursor()
         c.execute("CREATE TABLE labelType (Name TEXT PRIMARY KEY, Color INTEGER, Description TEXT)")
-        c.execute("CREATE TABLE labelData (Start_time REAL, Label_name TEXT, Sensor_id TEXT, "
+        c.execute("CREATE TABLE labelData (Start_time REAL, End_time REAL, Label_name TEXT, Sensor_id TEXT, "
                   "PRIMARY KEY(Start_time, Sensor_id), FOREIGN KEY (Label_name) REFERENCES labelType(Name))")
         self._conn.commit()
 
@@ -56,16 +56,17 @@ class LabelManager:
         self._cur.execute(sql_del_label_type, [name])
         self._conn.commit()
 
-    def add_label(self, time: float, name: str, sensor: str) -> None:
+    def add_label(self, start_time: float, end_time: float, name: str, sensor: str) -> None:
         """
         Adds a label to the data of a sensor.
 
-        :param time: The timestamp in the sensor-data at which the label starts
+        :param start_time: The timestamp in the sensor-data at which the label starts
+        :param end_time: The The timestamp in the sensor-data at which the label ends
         :param name: The name of the label type that is used
         :param sensor: The sensor ID belonging to the data
         """
         try:
-            self._cur.execute(sql_add_label, (time, name, sensor))
+            self._cur.execute(sql_add_label, (start_time, end_time, name, sensor))
             self._conn.commit()
         except sqlite3.Error as e:
             # TODO: label at this time exists, give error message
@@ -124,12 +125,12 @@ class LabelManager:
         self._cur.execute(sql_change_label, (name, time, sens_id))
         self._conn.commit()
 
-    def get_all_labels(self, sensor_id: str) -> List[Tuple[float, str]]:
+    def get_all_labels(self, sensor_id: str) -> List[Tuple[float, float, str]]:
         """
         Returns all the labels for a given sensor.
 
         :param sensor_id: The sensor ID of the sensor for which the labels need to be returned
-        :return: List of all labels belonging to the sensor
+        :return: List of tuples (start_time, end_time, label_name) of all labels belonging to the sensor
         """
         self._cur.execute(sql_get_labels, [sensor_id])
         return self._cur.fetchall()
