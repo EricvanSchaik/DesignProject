7c13af1d78de85c1c28b5f3884ec391ccbe973cd
Semakon <semakon@gmail.com>
2018-09-30 16:31:44 +0200

    Parsing is functional, but not fully implemented

--- parse_string/Function.g4
+++ parse_string/Function.g4
@@ -1,16 +1,67 @@
 grammar Function;
 
-// Parser rules
-expr    : LBRACKET expr RBRACKET | expr OP expr
-        | sqrt | NUMBER | VAR;
-sqrt    : SQRT LBRACKET expr RBRACKET;
+/* Expressions */
+expr    : left=expr POWER right=expr    #powExpr
+        | left=expr multOp right=expr   #multExpr
+        | left=expr plusOp right=expr   #plusExpr
+        | SQRT LBRACKET expr RBRACKET   #sqrtExpr
+        | LBRACKET expr RBRACKET        #bracketExpr
+        | VAR                           #varExpr
+        | NUM                           #numExpr
+        ;
 
-// Lexer rules
-fragment DIGIT  : [0-9];
-NUMBER          : DIGIT+ ([.,] DIGIT+)?;
-SQRT            : 'sqrt';
-VAR             : ([A-Z]|[a-z])+;
-OP              : '+' | '-' | '*' | '/' | '^';
+/* Multipliative operator */
+multOp  : MULT | DIV;
+
+/* Addition operator */
+plusOp  : PLUS | MINUS;
+
+/* Reserved keywords */
+SQRT            : S Q R T;
+
+/* Symbols */
+MULT            : '*';
+DIV             : '/';
+PLUS            : '+';
+MINUS           : '-';
+POWER           : '^';
 LBRACKET        : '(';
 RBRACKET        : ')';
-WHITESPACE      : ' ' -> skip;
+
+/* Tokens with content */
+VAR             : LETTER (LETTER | DIGIT)*;
+NUM             : DIGIT+ ([.,] DIGIT+)?;
+
+fragment LETTER : [A-Za-z];
+fragment DIGIT  : [0-9];
+
+/* Skipped content */
+WHITESPACE      : [ \t\n\r] -> skip;
+
+/* Letter fragments */
+fragment A      : [aA];
+fragment B      : [bB];
+fragment C      : [cC];
+fragment D      : [dD];
+fragment E      : [eE];
+fragment F      : [fF];
+fragment G      : [gG];
+fragment H      : [hH];
+fragment I      : [iI];
+fragment J      : [jJ];
+fragment K      : [kK];
+fragment L      : [lL];
+fragment M      : [mM];
+fragment N      : [nN];
+fragment O      : [oO];
+fragment P      : [pP];
+fragment Q      : [qQ];
+fragment R      : [rR];
+fragment S      : [sS];
+fragment T      : [tT];
+fragment U      : [uU];
+fragment V      : [vV];
+fragment W      : [wW];
+fragment X      : [xX];
+fragment Y      : [yY];
+fragment Z      : [zZ];
--- parse_string/parser_test.py
+++ parse_string/parser_test.py
@@ -1,18 +1,62 @@
 from antlr4 import *
 from gen.FunctionLexer import FunctionLexer
 from gen.FunctionParser import FunctionParser
-from parse_string.basic_function_listener import BasicFunctionListener
+from parse_string.basic_function_visitor import BasicFunctionVisitor
+from parse_string.error_listener import FunctionErrorListener
+from parse_string.parse_exception import ParseException
 
 
-def test1(string):
-    tokens = CommonTokenStream(FunctionLexer(InputStream(string)))
-    parser = FunctionParser(tokens)
+def dict_test_data():
+    return {'Time': 0.006042,
+            'Ax': 4.922654,
+            'Ay': -1.163624,
+            'Az': -6.876393,
+            'Gx': 60.302734,
+            'Gy': -66.955566,
+            'Gz': -51.208496,
+            'Mx': -84.090243,
+            'My': -41.312628,
+            'Mz': -43.363610,
+            'T': 25.560,
+            'Vector': 8.536470}
+
+
+def parse_string(string):
+    # Build parser from string
+    parser = FunctionParser(CommonTokenStream(FunctionLexer(InputStream(string))))
+
+    # Add custom error listener to parser
+    parser._listeners = [FunctionErrorListener()]
     parser.buildParseTrees = True
-    tree = parser.expr()
-    listener = BasicFunctionListener()
-    ParseTreeWalker.DEFAULT.walk(listener, tree)
-    print(listener.string)
-    pass
+    try:
+        # Create parse tree
+        tree = parser.expr()
+
+        # Visit parse tree with custom visitor
+        visitor = BasicFunctionVisitor()
+        visitor.visit(tree)
+
+        # Print statement for debugging purposes
+        # print(visitor.debug)
+
+        # Return result string from visitor
+        return visitor.string
+    except ParseException as e:
+        # Catch parse exception and handle it
+        return e.args
 
 
-test1("1 + 2")
+def evaluate_parsed_string(row, string):
+    # Copy string
+    copy = string
+
+    # Check for every variable in row whether it's in the string
+    for var in row.keys():
+        if var in string:
+            # Replace variable name with its value in parentheses (for negative numbers)
+            copy = copy.replace(var, "(" + str(row[var]) + ")")
+    return eval(copy)
+
+
+parse_result = parse_string("sqrt(Ax^2 + Ay^2 + Az^2)")
+print(evaluate_parsed_string(dict_test_data(), parse_result))
