644df35f9e8a5ff2fedc78fa9903dc61c2b94506
Semakon <Semakon@gmail.com>
2018-10-01 15:39:08 +0200

    better documentation

--- parse_string/custom_function_parser.py
+++ parse_string/custom_function_parser.py
@@ -10,6 +10,20 @@ import pandas as pd
 class CustomFunctionParser:
 
     def __init__(self, expr: str, names: list):
+        """
+        The CustomFunctionParser parses a custom expression string into a string readable by
+        Python. It uses a list of names with possible variables to make replacing the variable
+        names with their values faster.
+        :param expr: The expression in string format. Syntax:
+            Plus:           '+'
+            Minus:          '-'
+            Multiply:       '*'
+            Divide:         '/'
+            Power:          '^'
+            Square Root:    'sqrt()'
+            Brackets:       '(', ')'
+        :param names: list of possible variable names that should be recognized.
+        """
         self.expr = expr
 
         # Determine variables used from names
@@ -53,7 +67,7 @@ class CustomFunctionParser:
     def evaluate_row(self, row: dict):
         """
         Replaces the variable names with their values taken from the row dictionary and evaluates
-        the resulting python expression.
+        the resulting python expression. Works for multiple variables
         :param row: Dictionary containing variable (column) names with their respective value
         :return: The result of the expression evaluated using eval()
         """
@@ -70,6 +84,7 @@ class CustomFunctionParser:
     def evaluate_single(self, x):
         """
         Replaces the variable name with its value and evaluates the resulting python expression.
+        Only works for a single variable.
         :param x: value of variable in parsed expression
         :return: The result of the expression evaluated using eval()
         """
