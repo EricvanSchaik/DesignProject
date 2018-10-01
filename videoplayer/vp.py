77038e69fcaac835a44eea7c7f74585f5b88a1f5
Eric <schaikejgv@gmail.com>
2018-10-01 11:59:51 +0200

    Opens dialog with label

--- videoplayer/vp.py
+++ videoplayer/vp.py
@@ -11,7 +11,7 @@ from data_import import import_data
 import video_metadata as vm
 from datetime import timedelta
 import matplotlib.animation
-import numpy as np
+from datastorage import LabelStorage
 from PyQt5 import QtCore
 from videoplayer.labelspecs import Ui_LabelSpecs
 
@@ -172,6 +172,44 @@ class LabelSpecs(QtWidgets.QDialog, Ui_LabelSpecs):
         super().__init__()
         # Initialize the generated UI from vpdesigner.py.
         self.setupUi(self)
+        self.label = Label()
+        self.doubleSpinBox_start.valueChanged.connect(self.start_changed)
+        self.doubleSpinBox_end.valueChanged.connect(self.stop_changed)
+        # self.accepted.connect(self.send_label)
+        # self.comboBox_labels.activated.connect(self.label_changed)
+        # self.label_storage = LabelStorage.LabelManager("stub")
+
+    def start_changed(self, value: float):
+        self.label.start = value
+
+    def stop_changed(self, value: float):
+        self.label.stop = value
+
+    def label_changed(self, label: str):
+        self.label.label = label
+
+    # def send_label(self):
+    #     self.label_storage.add_label(self.label.start, self.label.end, self.label.label, "")
+
+    # def get_label(self, number: int):
+    #     pass
+
+
+class Label:
+
+    def __init__(self):
+        self.label = 0
+        self.start = 0.00
+        self.end = 0.00
+
+    def setLabel(self, name: str):
+        self.label = name
+
+    def setStart(self, start: float):
+        self.start = start
+
+    def setEnd(self, end: float):
+        self.end = end
 
 
 if __name__ == '__main__':
