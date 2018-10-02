fc1c102c68357e7a221dd5c8520896b608eb2181
Semakon <Semakon@gmail.com>
2018-10-02 15:50:44 +0200

    gen map for parse helper files added

--- parse_string/function_parse_unittest.py
+++ parse_string/function_parse_unittest.py
@@ -1,9 +1,15 @@
 import unittest
+import pandas as pd
 from parse_string import custom_function_parser as cfp
 from parse_string.parse_exception import ParseException
 
 
 class FunctionParseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.df = pd.DataFrame({'A': [1, 2, 3],
+                                'B': [4, 5, 6],
+                                'C': [7, 8, 9]})
 
     def test_correct_syntax(self):
         self.assertEqual("5+5", cfp.parse("5 + 5"),
@@ -30,3 +36,26 @@ class FunctionParseTestCase(unittest.TestCase):
         self.assertRaises(ParseException, cfp.parse, "- 4")
         self.assertRaises(ParseException, cfp.parse, "sqrt 5")
         self.assertRaises(ParseException, cfp.parse, "4^")
+
+    def test_pd_eval(self):
+        # Calculate mean
+        parse_string = cfp.parse("(A + B + C) / 3")
+        self.df.eval("D = " + parse_string, inplace=True)
+
+        self.assertEqual((1 + 4 + 7) / 3, self.df['D'][0],
+                         "Mean of A, B, and C incorrectly calculated")
+        self.assertEqual((2 + 5 + 8) / 3, self.df['D'][1],
+                         "Mean of A, B, and C incorrectly calculated")
+        self.assertEqual((3 + 6 + 9) / 3, self.df['D'][2],
+                         "Mean of A, B, and C incorrectly calculated")
+
+        # Calculate l2-norm
+        parse_string = cfp.parse("sqrt(A^2 + B^2 + C^2)")
+        self.df.eval("E = " + parse_string, inplace=True)
+
+        self.assertEqual((1**2 + 4**2 + 7**2)**0.5, self.df['E'][0],
+                         "l2-norm of A, B, and C incorrectly calculated")
+        self.assertEqual((2**2 + 5**2 + 8**2)**0.5, self.df['E'][1],
+                         "l2-norm of A, B, and C incorrectly calculated")
+        self.assertEqual((3**2 + 6**2 + 9**2)**0.5, self.df['E'][2],
+                         "l2-norm of A, B, and C incorrectly calculated")
