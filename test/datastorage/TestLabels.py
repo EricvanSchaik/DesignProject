55672f375ef0566475b262d6f20196968063dab9
Martijn <martijnrenssen@live.nl>
2018-10-12 12:00:32 +0200

    update test

--- test/datastorage/TestLabels.py
+++ test/datastorage/TestLabels.py
@@ -17,7 +17,7 @@ class TestLabels(unittest.TestCase):
         self.l.add_label_type('label1', 420, 'This is a test label')     # add new label type with name 'label1'
         self.assertNotEqual(0, len(
             self.l._cur.execute('SELECT * FROM labelType').fetchall()))  # new label type should be in the table
-        self.l.add_label(1, 'label1', 'sensor1')                         # create a label with the new type
+        self.l.add_label(datetime.now(), datetime.now(), 'label1', 'sensor1')  # create a label with the new type
         self.l.delete_label_type('label1')
         self.assertEqual(0, len(
             self.l._cur.execute('SELECT * FROM labelType').fetchall()))  # label type should not be in the table
@@ -25,16 +25,18 @@ class TestLabels(unittest.TestCase):
             self.l._cur.execute('SELECT * FROM labelData').fetchall()))  # label should not be in the table
 
     def test_add_del_label(self):
-        self.l.add_label(1.5, 'label1', 'sensor1')                       # add new label at time 1.5 to sensor 'sensor1'
+        label_time = datetime.now()
+        self.l.add_label(label_time, label_time, 'label1', 'sensor1')    # add new label at time 1.5 to sensor 'sensor1'
         self.assertNotEqual(0, len(
             self.l._cur.execute('SELECT * FROM labelData').fetchall()))  # new label should be in the table
-        self.l.delete_label(1.5, 'sensor1')
+        self.l.delete_label(label_time, 'sensor1')
         self.assertEqual(0, len(
             self.l._cur.execute('SELECT * FROM labelData').fetchall()))  # label should not be in the table
 
     def test_update_label_type(self):
-        self.l.add_label_type('label1', 420, 'This is a test label')  # add new label type with name 'label1'
-        self.l.add_label(1, 'label1', 'sensor1')                      # create a label with the new type
+        label_time = datetime.now()
+        self.l.add_label_type('label1', 420, 'This is a test label')   # add new label type with name 'label1'
+        self.l.add_label(label_time, label_time, 'label1', 'sensor1')  # create a label with the new type
         self.assertEqual('label1', self.l._cur.execute('SELECT Name FROM labelType')
                          .fetchone()[0])  # label type name should be 'label1'
         self.assertEqual(420, self.l._cur.execute('SELECT Color FROM labelType')
