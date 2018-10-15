a9967973e8d225eb85b7dde3efe8b36477516328
Martijn <martijnrenssen@live.nl>
2018-10-15 13:49:50 +0200

    don't return sensorID

--- datastorage/labelstorage.py
+++ datastorage/labelstorage.py
@@ -16,7 +16,8 @@ sql_upd_desc = "UPDATE labelType SET Description = ? WHERE Name = ?"
 sql_change_label = "UPDATE labelData SET Label_name = ? WHERE Start_time = ? AND Sensor_id = ?"
 sql_get_labels = "SELECT Start_time, End_time, Label_name FROM labelData WHERE Sensor_id = ? ORDER BY Start_time ASC"
 sql_get_all_labels = "SELECT * FROM labelData ORDER BY Start_time ASC"
-sql_get_labels_date = "SELECT * FROM labelData WHERE date(Start_time) = ? AND Sensor_id = ? ORDER BY Start_time ASC"
+sql_get_labels_date = "SELECT Start_time, End_time, Label_name FROM labelData WHERE date(Start_time) = ? AND " \
+                      "Sensor_id = ? ORDER BY Start_time ASC"
 
 
 class LabelManager:
@@ -153,7 +154,7 @@ class LabelManager:
         self._cur.execute(sql_get_labels, [sensor_id])
         return self._cur.fetchall()
 
-    def get_labels_date(self, sensor_id: str, date: date):
+    def get_labels_date(self, sensor_id: str, date: date) -> List[Tuple[datetime, datetime, str]]:
         """
         Returns all the labels for a given sensor on a given date.
 
