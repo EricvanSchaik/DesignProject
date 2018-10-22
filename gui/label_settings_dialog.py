d7302559aa953a43b85764f0651efa3d5b74c94c
Eric <schaikejgv@gmail.com>
2018-10-22 21:20:19 +0200

    You can delete label types.

--- gui/label_settings_dialog.py
+++ gui/label_settings_dialog.py
@@ -10,9 +10,10 @@ class LabelSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
         super().__init__()
         self.setupUi(self)
         self.label_manager = label_manager
-        self.lineEdit.returnPressed.connect(self.add_label)
+        self.accepted.connect(self.add_label)
         self.pushButton.clicked.connect(self.delete_label)
         self.comboBox.currentTextChanged.connect(self.label_changed)
+        self.comboBox_2.currentTextChanged.connect(self.color_changed)
         colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white']
         self.comboBox_2.addItems(colors)
         self.comboBox_3.addItems(colors)
@@ -24,16 +25,23 @@ class LabelSettingsDialog(QtWidgets.QDialog, Ui_Dialog):
             self.comboBox_2.setCurrentText(self.label_manager.get_label_types()[0][1])
 
     def add_label(self):
-        self.label_manager.add_label_type(self.lineEdit.text(), self.comboBox_3.currentText(), '')
+        if self.lineEdit.text():
+            self.label_manager.add_label_type(self.lineEdit.text(), self.comboBox_3.currentText(), '')
+
+    def delete_label(self):
+        self.label_manager.delete_label_type(self.comboBox.currentText())
+        self.comboBox.clear()
         for label in self.label_manager.get_label_types():
             self.comboBox.addItem(label[0])
             self.color_dict[label[0]] = label[1]
-        self.comboBox_2.setCurrentText(self.label_manager.get_label_types()[0][1])
-        self.lineEdit.clear()
-
-    def delete_label(self):
-        pass
+        if self.label_manager.get_label_types():
+            self.comboBox_2.setCurrentText(self.label_manager.get_label_types()[0][1])
 
     def label_changed(self, text):
-        if self.color_dict:
+        if self.color_dict and self.comboBox.count():
             self.comboBox_2.setCurrentText(self.color_dict[text])
+
+    def color_changed(self, color):
+        if self.comboBox.currentText():
+            self.label_manager.update_label_color(self.comboBox.currentText(), color)
+            self.color_dict[self.comboBox.currentText()] = color
