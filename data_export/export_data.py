2d8de86ab5efd10d9a030277cfbebef9b930e295
Semakon <Semakon@gmail.com>
2018-11-02 12:14:51 +0100

    Export unittest and documentation

--- data_export/export_data.py
+++ data_export/export_data.py
@@ -5,6 +5,19 @@ from data_export import windowing as w
 
 
 def export(data: [], label_col: str, timestamp_col: str, file_path: str, comments: [str], comment=';'):
+    """
+    Exports a list of DataFrames after using the windowing_fast method to get a statistical
+    overview of the data contained in the DataFrames. The DataFrames are required to have
+    a column for labels and a column for timestamps. All other columns will be used for
+    windowing.
+    :param data: The DataFrames that are exported.
+    :param label_col: The column containing the labels.
+    :param timestamp_col: The column containing the timestamps.
+    :param file_path: The path of the newly created file.
+    :param comments: A list of comments that will be added at the head of the file.
+    :param comment: The style used to denote comments (defaults to ';')
+    :return:
+    """
     # Initialise result list
     res = []
 
